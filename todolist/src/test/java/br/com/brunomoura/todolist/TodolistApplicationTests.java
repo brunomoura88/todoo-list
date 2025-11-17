fd421a8696a376159396a8bec0205e8e6bbc3e90
Bruno Moura <bruno.sdemoura@gmail.com>
2025-11-16 21:15:35 -0300

    Atualização

---
+++ todolist/src/test/java/br/com/brunomoura/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunomoura.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
